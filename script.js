69cb02a0fd10223aeab658b101137c452eaf0cc3
jontanxy <ce30116@gmail.com>
2026-07-13 14:50:15 +0800

    Shifted files

--- script.js
+++
@@ -1,6 +0,0 @@
-const year = new Date().getFullYear();
-const footerText = document.querySelector("footer p");
-
-if (footerText) {
-  footerText.textContent = `© ${year} ResearchVault. Built for secure collaborative research.`;
-}
